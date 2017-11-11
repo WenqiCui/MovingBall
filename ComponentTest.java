6e915d7ea25396d9241cf72fad7bbb308bd8a003
WenqiCui <31945924+WenqiCui@users.noreply.github.com>
2017-11-10 23:03:24 -0500

    Base

---
+++ ComponentTest.java
@@ -0,0 +1,19 @@
+package demoBall.MovingBall;
+
+import java.awt.EventQueue;
+
+public class ComponentTest {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		EventQueue.invokeLater(()->{
+			MyFrame frame=new MyFrame();
+			//frame.add(new MyTextPanel());
+			//frame.addKeyListener(bp);
+			
+			
+		});
+
+	}
+
+}
